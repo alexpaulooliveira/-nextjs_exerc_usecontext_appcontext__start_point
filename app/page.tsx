6350bc82035e2ee7e4ab41b90fcb60be662a621b
alexpaulooliveira <alexpaulo@cpatc.embrapa.br>
2023-05-25 14:59:58 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const Home = () => {
+  return (
+    <div className='text-center pt-10'> 
+      Desenvolver a aplicação aqui
+    </div>
+  );
+};
+
+export default Home
